3868ebb91d45efee3a3367b136ae49a042b74570
Ekaterina Strauzova <101550553+strauzova@users.noreply.github.com>
2022-09-12 17:48:46 +0300

    Added a Homework6

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Write("Enter quantity of numbers: ");
+
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[quantity];
+    
+int count = 0;
+
+void FillNumbers (int[] array)
+{
+    Console.Write("Enter the numbers: ");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+               
+
+        if (array[i] > 0) count++;  
+
+    }
+    
+    if (count > 0) Console.WriteLine($"Positive numbers in this array are: {count}");
+    else Console.WriteLine("There are no positive numbers in this array");
+
+}
+
+
+
+FillNumbers(arr);
+
+
---
+++ Homework6/Task43/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.Write("Enter a constant coefficient b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a coefficient k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a constant coefficient b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a coefficient k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+// Вывести x с помочью их этго уравнения:
+// k1 * x + b1 = k2 * x + b2
+// x (k1 - k2) = b2 - b1
+// x = (b2 - b1) / (k2 - k1)
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"The point intersection of lines is ({x}; {y})");
+
+
