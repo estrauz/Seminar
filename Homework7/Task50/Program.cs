59a7b9f49e95ade18f4836f9fd0303b12a292dd2
Ekaterina Strauzova <101550553+strauzova@users.noreply.github.com>
2022-09-15 19:54:11 +0300

    Added Homework7

---
+++ Homework7/Task50/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном
+массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4 
+17 -> такого числа в массиве нет */
+
+Console.Write("Enter a number of row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a number of column: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[row,column];
+
+RandomMatrix(matrix);
+
+Console.Write("Request the element: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+FindElement(matrix);
+
+void RandomMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 100); // [0;99]
+            Console.Write($"{matr[i,j]}  ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FindElement (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (number == matr[i,j]) 
+            {
+                Console.WriteLine($"{matr[i,j]} - the number included in the matrix");
+            }
+            else Console.WriteLine($"{number} - that number NOT included in the matrix");
+        }
+
+    }
+}    
+
+    
+    
+    
+
+
---
+++ Homework7/Task52/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+Console.Write("Enter a number of row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a number of column: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[row,column];
+
+FillArray(matrix);
+FindAvg(matrix);
+
+double result = 0;
+double AverageCol = 0;
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 20); // [0;19]
+            Console.Write($"{matr[i,j]}  ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FindAvg(double[,] matr)
+{
+    Console.Write("The average of columns is: ");
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        for (int i = 0; i < 1; j++)
+            {
+                result = result + matrix(i,j);
+            }
+    }
+    AverageCol = result / matrix.GetLength(1);
+}
+
+
+
+
+
